0ab0e719bdf8a1e7d1e1158ef02068c3a09fd3ef
LeoSeiti <Leonardoseiti1@gmail.com>
2024-05-30 13:07:50 -0300

    another changes

---
+++ src/main/java/br/com/fiap/blueocean/model/Coral.java
@@ -0,0 +1,20 @@
+package br.com.fiap.blueocean.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "T_CORAL")
+public class Coral {
+    //Tipos de corais são: 
+    //Soft, LPS - coral duro com pólipos grandes e SPS - coral duro com pólipos pequenos
+    @NotBlank(message = "{coral.tipo.notblank}")
+    @Size(min = 3, max = 4, message = "{coral.tipo.size}")
+    private String tipo;
+    @NotBlank(message = "{coral.cor.notblank}")
+    private String cor;
+}
